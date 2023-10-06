9a62789b559b319d1fc4d185ea17891ed2537b75
khesehang <khesehangsamsohang004@gmail.com>
2023-10-06 19:47:32 +0545

    scrolling hills1 revamp

--- script.js
+++ script.js
@@ -7,11 +7,16 @@ let hill5 = document.getElementById('hill5');
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
     console.log(value)
+        text.style.marginTop = value * 2.5 + 'px'
+        leaf.style.top = value * -1.5 + 'px'
+        leaf.style.left = value * 1.5 + 'px'
+        hill5.style.left = value * 1.5 + 'px'
+        hill4.style.left = value * -1.5 + 'px'
+        hill1.style.top = value * 1 + 'px'
 
-    text.style.marginTop = value * 2.5 + 'px'
-    leaf.style.top = value * -1.5 + 'px'
-    leaf.style.left = value * 1.5 + 'px'
-    hill5.style.left = value * 1.5 + 'px'
-    hill4.style.left = value * -1.5 + 'px'
-    hill1.style.top = value * 1 + 'px'
+        if(value > 500) {
+            hill1.style.display = 'none'
+        } else {
+            hill1.style.display = 'block'
+        }
 })
